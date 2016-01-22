69961594c8e67457183aecffec5f68fd7f344596
rodrigues-am <andremr@if.usp.br>
2016-01-22 11:48:03 -0200

    Var controle retiradas do INEP-MEC

---
+++ R/var_controle.R
@@ -0,0 +1,9 @@
+# Carrega variáveis pré definidas pelo MEC/INEP
+
+# Definindo variáveis de controle
+mat.code<- "145F18" # Curdo de Matemática licenciatura
+fis.code<- "145F09" # Curdo de Física licenciatura
+cie.code<- "145F02" # Curdo Ciências licenciatura
+pedl.code<- "142P01" # Curdo de Pedagogia licenciatura
+pedb.code<- "142C01" # Curdo de Pedagogia licenciatura
+usp.code<- "55" # Código da USP no MEC
